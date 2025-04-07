7bfa8f2beb4d9689a73aa7096e4c050a32229b76
ctvnjhnrmmlp <johnrommel.octaviano@gmail.com>
2025-04-07 19:41:43 +0800

    fix: updated providers for react query

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,8 +1,21 @@
 'use client';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
 import { ReactNode } from 'react';
 
+const query = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 export default function Providers({ children }: { children: ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <QueryClientProvider client={query}>{children}</QueryClientProvider>
+    </SessionProvider>
+  );
 }
